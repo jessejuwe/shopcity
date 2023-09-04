48680875246a80939330ae05db1fb76694b8d5d7
jessejuwe <jessejuwe@gmail.com>
2023-09-04 17:20:30 +0100

    9th commit (updated app)

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+const hash = require('hash-it');
 
 require('dotenv').config();
 
@@ -17,24 +20,56 @@ const shopRoutes = require('./src/routes/shop');
 const errorController = require('./src/controllers/error');
 const User = require('./src/models/mongoose/user');
 
-const { get404 } = errorController;
+const { get404, get500 } = errorController;
 
 const app = express();
+
 const store = new MongoDBStore({
   uri: process.env.MONGO_CLIENT,
   collection: 'sessions',
 });
+
 const csrfProtection = csrf();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    try {
+      cb(null, `${uuidv4()}-${file.originalname}`);
+      // cb(null, `${hash({ name: file.originalname })}-${file.originalname}`);
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 // configuring templating engine
 app.set('view engine', 'ejs'); // using ejs as the templating engine
 app.set('views', './src/views');
 
-// registering a middleware for parsing body
+// registering a middleware for parsing body (form data as text)
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// registering a middleware for body (multi-part data)
+app.use(multer({ storage, fileFilter }).single('image'));
 
 // registering a middleware for static files (for importing files)
 app.use(express.static(path.join(rootDir, 'public')));
+app.use('/images', express.static(path.join(rootDir, 'images')));
 
 // registering a middleware for initializing sessions
 app.use(
@@ -52,18 +87,6 @@ app.use(csrfProtection);
 // registering a middleware for exposing middlewares
 app.use(flash());
 
-// registering a middleware for storing user in request
-app.use((req, res, next) => {
-  if (!req.session.user) return next();
-
-  User.findById(req.session.user._id)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
-
 // registering a middleware for storing local variables in request
 app.use((req, res, next) => {
   res.locals.isLoggedIn = req.session.isLoggedIn;
@@ -72,12 +95,42 @@ app.use((req, res, next) => {
   next();
 });
 
+// registering a middleware for storing user in request
+app.use((req, res, next) => {
+  if (!req.session.user) return next();
+
+  User.findById(req.session.user._id)
+    .then(user => {
+      // throw new Error('No User found'); // used for throwing error in synchronous code
+      if (!user) next();
+
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      next(new Error(err)); // used for throwing error in asynchronous code
+    });
+});
+
 app.use('/admin', adminRoutes.router); // middleware for out-sourced routes
 app.use(shopRoutes); // middleware for out-sourced routes
 app.use(authRoutes); // middleware for out-sourced routes
 
-// creating a middleware for handling error pages
+// creating a middleware for handling technical errors
+app.use(get500);
+
+// creating a middleware for handling "page not found" errors
 app.use(get404);
+
+// registering an error-handling middleware
+app.use((error, req, res, next) => {
+  // res.redirect('/500');
+  return res.status(500).render('500', {
+    pageTitle: '500 - Technical issue',
+    path: '/500',
+    isLoggedIn: req.session.isLoggedIn,
+  });
+});
 
 // Mongoose connection
 mongoose
